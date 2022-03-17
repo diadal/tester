ad9af3c0d5900fd892d7ae93fa2ffd7f94d4a7c6
Diadal <info@diadal.com.ng>
2022-03-17 05:39:17 -0700

    update

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+   <q-btn label="Take Picture" @click="takePicture"/>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Camera, CameraResultType } from '@capacitor/camera';
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup () {
+
+     const takePicture = async () => {
+       const image = await Camera.getPhoto({
+            quality: 90,
+            allowEditing: true,
+            resultType: CameraResultType.Uri,
+          });
+          const imageUrl = image.webPath;
+          console.log('imageUrl: ', imageUrl);
+    };
+
+    return {
+takePicture
+
+     };
+  }
+});
+</script>
